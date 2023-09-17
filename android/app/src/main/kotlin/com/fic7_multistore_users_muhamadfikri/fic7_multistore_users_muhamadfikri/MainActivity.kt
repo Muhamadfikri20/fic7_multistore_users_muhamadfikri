1de84c1ecdddcd1dba359fb131760a93cef9c311
Muhamadfikri20 <muhamadfikri9602@gmail.com>
2023-09-17 21:36:28 +0700

    First Commit

---
+++ android/app/src/main/kotlin/com/fic7_multistore_users_muhamadfikri/fic7_multistore_users_muhamadfikri/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.fic7_multistore_users_muhamadfikri.fic7_multistore_users_muhamadfikri
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
